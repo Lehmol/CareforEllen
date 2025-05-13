2867deebc045d5f4363f1823ae4fc8ef44ac5861
Lehmol <chalottelinnea@hotmail.com>
2025-05-13 10:50:03 +0200

    Changes in Header and Homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
 function Header() {
   return (
     <>
       <header>
         <div>
-          <p>1 August 2025 - 09:05</p>
+          <img />
+          <h1>Good morning Sofia!</h1>
         </div>
-        <h1>Care for Ellen</h1>
       </header>
     </>
   );
